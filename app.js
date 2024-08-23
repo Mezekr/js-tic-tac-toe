49199a32b7b0cc7ca22f472914ab53f431042c60
Mezekr W <mezi.edu@tuta.io>
2024-08-23 21:08:25 +0200

    add a function to check the winner

--- app.js
+++ app.js
@@ -22,6 +22,48 @@ const addGo = (e) => {
 	go = go === 'circle' ? 'cross' : 'circle';
 	infoDisplay.textContent = `Now is turn for ${go.toUpperCase()}!`;
 	e.target.removeEventListener('click', addGo);
+	checkMoves();
+};
+
+const checkMoves = () => {
+	const winningCombos = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 5],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	const allCellElements = document.querySelectorAll('.cell-element');
+	console.log(allCellElements);
+
+	winningCombos.forEach((arr) => {
+		const isCircleWins = arr.every((cell) =>
+			allCellElements[cell].firstChild?.classList.contains('circle')
+		);
+
+		if (isCircleWins) {
+			infoDisplay.textContent = `Cirle Wins!`;
+			allCellElements.forEach((cellElement) =>
+				cellElement.replaceWith(cellElement.cloneNode(true))
+			);
+		}
+	});
+
+	winningCombos.forEach((arr) => {
+		const isCrossWins = arr.every((cell) =>
+			allCellElements[cell].firstChild?.classList.contains('cross')
+		);
+
+		if (isCrossWins) {
+			infoDisplay.textContent = `Cross Wins!`;
+			allCellElements.forEach((cellElement) =>
+				cellElement.replaceWith(cellElement.cloneNode(true))
+			);
+		}
+	});
 };
 
 createGameBoard();
